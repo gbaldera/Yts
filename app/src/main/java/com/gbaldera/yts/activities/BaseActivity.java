64c4da646330d16af17f65576ccdffafd1fdb7d5
Gustavo Rodriguez Baldera <gbaldera@gmail.com>
2014-11-11 22:59:52 -0400

    Setting up the base interface for the app

--- app/src/main/java/com/gbaldera/yts/activities/BaseActivity.java
+++ app/src/main/java/com/gbaldera/yts/activities/BaseActivity.java
@@ -1,7 +1,86 @@
 package com.gbaldera.yts.activities;
 
 
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
 
-public class BaseActivity extends ActionBarActivity {
+import com.gbaldera.yts.R;
+
+public abstract class BaseActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setupActionBar();
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        getActionBarToolbar();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        setupDrawer();
+    }
+
+    private void setupDrawer(){
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (mDrawerLayout == null) {
+            return;
+        }
+
+        if(mToolbar != null){
+            mToolbar.setNavigationIcon(R.drawable.ic_drawer);
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mDrawerLayout.openDrawer(Gravity.START);
+                }
+            });
+
+            mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                    R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            mDrawerLayout.setDrawerListener(mDrawerToggle);
+            mDrawerToggle.syncState();
+        }
+        else{
+            mDrawerToggle = null;
+            mDrawerLayout.setDrawerListener(null);
+        }
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+    }
+
+    protected Toolbar getActionBarToolbar() {
+        if (mToolbar == null) {
+            mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+            if (mToolbar != null) {
+                setSupportActionBar(mToolbar);
+            }
+        }
+        return mToolbar;
+    }
+
+    protected void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 }
--- app/src/main/java/com/gbaldera/yts/activities/MainActivity.java
+++ app/src/main/java/com/gbaldera/yts/activities/MainActivity.java
@@ -1,18 +1,68 @@
 package com.gbaldera.yts.activities;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.gbaldera.yts.R;
+import com.gbaldera.yts.fragments.TabFragment;
+import com.gbaldera.yts.widgets.SlidingTabLayout;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements TabFragment.OnFragmentInteractionListener {
+
+    ViewPager mViewPager = null;
+    MainTabsViewPagerAdapter mViewPagerAdapter = null;
+    SlidingTabLayout mSlidingTabLayout = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        mViewPagerAdapter = new MainTabsViewPagerAdapter(getFragmentManager());
+        mViewPager.setAdapter(mViewPagerAdapter);
+
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setCustomTabView(R.layout.tab_indicator, android.R.id.text1);
+
+        // add tabs
+        mSlidingTabLayout.setContentDescription(0, getString(R.string.tab_latest));
+        mSlidingTabLayout.setContentDescription(0, getString(R.string.tab_popular));
+        mSlidingTabLayout.setContentDescription(0, getString(R.string.tab_upcoming));
+
+        Resources res = getResources();
+        mSlidingTabLayout.setSelectedIndicatorColors(res.getColor(R.color.tab_selected_strip));
+        mSlidingTabLayout.setDistributeEvenly(true);
+        mSlidingTabLayout.setViewPager(mViewPager);
+
+        if (mSlidingTabLayout != null) {
+            mSlidingTabLayout.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset,
+                                           int positionOffsetPixels) {
+
+                }
+
+                @Override
+                public void onPageSelected(int position) {
+
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+                }
+            });
+        }
     }
 
 
@@ -36,5 +86,41 @@ public class MainActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    private class MainTabsViewPagerAdapter extends FragmentPagerAdapter {
+
+        public MainTabsViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            TabFragment frag = TabFragment.newInstance("Texto", "");
+            return frag;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 1:
+                    return getString(R.string.tab_popular);
+                case 2:
+                    return getString(R.string.tab_upcoming);
+                default:
+                    return getString(R.string.tab_latest);
+            }
+        }
     }
 }
